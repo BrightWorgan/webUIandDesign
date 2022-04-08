4dc9e7d6bdc3ae03b1b8beca75218b65c9200bcd
Sarah <sarah@cleancodecompany.com>
2022-04-08 16:24:16 +0100

    added the change text function

--- javaScript/Examples - getElementByID/Example 1/alumni.js
+++ javaScript/Examples - getElementByID/Example 1/alumni.js
@@ -1,11 +1,20 @@
 function changeText() {
-  document.getElementById("welcome").innerHTML = "Hi";
-  var greet = document.getElementById("events").innerHTML;
-  alert(greet);
-}
+  var x = document.getElementById("party");
+  if (x.innerHTML === "The Alummi Association's first Christmas Party will be held on Saturday 19th December") {
+    x.innerHTML = "This year’s Christmas Party will be held on Saturday 17th December";
+  } else {
+    x.innerHTML = "The Alummi Association's first Christmas Party will be held on Saturday 19th December";
+  }
 
-function changePic() {);
-}
+  function changePic() {;
+  }
 
-function addText() {;
+  function addText() {
+    var y = document.getElementById("extra");
+    if (y.innerHTML === " ") {
+      y.innerHTML = "\nHappy Christmas!";
+    } else {
+      y.innerHTML = " ";
+    }
+  }
 }
