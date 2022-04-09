d10f125b50f08221aff5540dc53f47b60263182e
Sarah <sarah@cleancodecompany.com>
2022-04-09 17:13:54 +0100

    added an alert

--- javaScript/Examples - getElementByID/Example 1/alumni.js
+++ javaScript/Examples - getElementByID/Example 1/alumni.js
@@ -1,20 +1,22 @@
+var isFirstChristmasParty = true
+
 function changeText() {
   var x = document.getElementById("party");
-  if (x.innerHTML === "The Alummi Association's first Christmas Party will be held on Saturday 19th December") {
+  x.style.backgroundColor = "grey"
+  if (isFirstChristmasParty) {
     x.innerHTML = "This year’s Christmas Party will be held on Saturday 17th December";
   } else {
     x.innerHTML = "The Alummi Association's first Christmas Party will be held on Saturday 19th December";
   }
+  isFirstChristmasParty = !isFirstChristmasParty
 
-  function changePic() {;
-  }
+}
 
-  function addText() {
-    var y = document.getElementById("extra");
-    if (y.innerHTML === " ") {
-      y.innerHTML = "\nHappy Christmas!";
-    } else {
-      y.innerHTML = " ";
-    }
-  }
+function changePic() {;
+}
+
+function addText() {
+
+  alert("Happy Christmas!");
+
 }
