dc5678ab652cba9f5f9d4ff7be06acdb1cbaae70
Sarah <sarah@cleancodecompany.com>
2022-04-19 20:21:42 +0100

    added a store user password function

--- javaScript/Examples - getElementByID/guessPasswordLab/guess.js
+++ javaScript/Examples - getElementByID/guessPasswordLab/guess.js
@@ -1,22 +1,34 @@
 let stroredPass = null
 
+// Store User's Password
+function storePassword() {
+  passInput = document.getElementById("password");
 
-function storePassword(document.getElementById("password");) {
-  storedPass = password
+  storedPass = passInput.value;
+
 }
 
+// Changes Styleing on Buttons
+function hideBox() {
+  if document.getElementById("storedPass") = display: hidden
+
+}
+
+
+// Check User's Password 
+var count = 0
 
 
 var isPasswordCorrect = true
 
-function addPasswordText() {
+function checkPassword() {
   var x = document.getElementById("textArea");
 
   if (isPasswordCorrect) {
-    x.innerHTML = "Incorrect. Try again, guesses: n. ";
+    x.innerHTML = "Incorrect. Try again, guesses: " + count;
   } else {
-    x.innerHTML = "Correct, that took n guesses’";
+    x.innerHTML = "Correct, that took" + count + " guesses";
   }
   isPasswordCorrect = !isPasswordCorrect
-
+  count += 1
 }
