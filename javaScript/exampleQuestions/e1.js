fd1cf60cea51a87ac368299b9532f4283aabd86e
Sarah <sarah@cleancodecompany.com>
2022-04-21 20:06:15 +0100

    prompt function start

---
+++ javaScript/exampleQuestions/e1.js
@@ -0,0 +1,21 @@
+promptUserHover() {
+  let userName = prompt("");
+
+  // if userMouse hovers over btn1;
+  // do this 
+  // if userMouse hovers over btn1;
+  // do this 
+  // if userMouse hovers over btn1;
+  // do this   
+  // else ;
+  // do this 
+  // alert("Hello " + userName);
+}
+
+
+promptUserClickbtn() {
+  let userName = prompt("");
+
+  // if click me btn is clicked;
+  // alert("");
+}
